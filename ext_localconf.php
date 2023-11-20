657f9a2b71a9296cbf2b065c34d7f3a702b23e13
Dominik Strolz <strolzdominik@gmx.net>
2023-11-20 09:11:14 +0100

    Disable cHash only in ted3-types

--- ext_localconf.php
+++ ext_localconf.php
@@ -15,14 +15,13 @@ if (TYPO3_MODE === 'BE') {
     } else {
         $PATH_site = PATH_site;
     }
-      
+
     if (!is_dir($PATH_site . "/fileadmin/ted3")) {
-        
+
         @mkdir($PATH_site . "/fileadmin/ted3");
         @mkdir($PATH_site . "/fileadmin/ted3/js");
-
     }
-  
+
     if (!file_exists($PATH_site . "/fileadmin/ted3/js/ted3-user-override.js")) {
         //  echo "notex"; exit;
         @copy($PATH_site . "/typo3conf/ext/ted3/Resources/Public/Dummy/ted3-user-override.js", $PATH_site . "/fileadmin/ted3/js/ted3-user-override.js");
@@ -38,15 +37,18 @@ if (TYPO3_MODE === 'BE') {
         ),
         // non-cacheable actions
         array(
-    \DS\Ted3\Controller\CrudController::class => 'tce,movecontent,delete,copy,createcontent,hide,translate,sort,addflexelement,copycontent,settings,test',
-    \DS\Ted3\Controller\BackendController::class => 'module,route,link',
+            \DS\Ted3\Controller\CrudController::class => 'tce,movecontent,delete,copy,createcontent,hide,translate,sort,addflexelement,copycontent,settings,test',
+            \DS\Ted3\Controller\BackendController::class => 'module,route,link',
 //    \DS\Ted3\Controller\NewContentElementController::class => 'wizard'
         )
 );
 $GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] .= ',ted3text1,ted3text2,ted3text3';
-$GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'] =  $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'].",webp";
+$GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'] = $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'] . ",webp";
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFoundOnCHashError'] = 0;
+
+if (in_array($_GET['type'], array(4455, 4456, 4457, 4500, 777, 4457))) {
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFoundOnCHashError'] = 0;
+}
 
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['configArrayPostProc'][] = \DS\Ted3\Hooks\FrontendRenderer::class . '->tsfe';
@@ -56,19 +58,19 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['configArr
 if (TYPO3_MODE === 'BE') {
     call_user_func(
             function ($extKey) {
-        // Get the extension configuration
-        $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey]);
+                // Get the extension configuration
+                $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey]);
 
-        if (!isset($extConf['loadted3ContentElementWizardTsConfig']) || (int) $extConf['loadted3ContentElementWizardTsConfig'] === 1) {
-            // Include new content elements to modWizards
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ted3/Configuration/TypoScript/pageTS.ts">');
-        }
+                if (!isset($extConf['loadted3ContentElementWizardTsConfig']) || (int) $extConf['loadted3ContentElementWizardTsConfig'] === 1) {
+                    // Include new content elements to modWizards
+                    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ted3/Configuration/TypoScript/pageTS.ts">');
+                }
 
-        $dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-        $dispatcher->connect(
-                \TYPO3\CMS\Extensionmanager\Controller\ConfigurationController::class, 'afterExtensionConfigurationWrite', \TYPO3\CMS\FluidStyledContent\Hooks\TcaCacheClearing::class, 'clearTcaCache'
-        );
-    }, $_EXTKEY
+                $dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+                $dispatcher->connect(
+                        \TYPO3\CMS\Extensionmanager\Controller\ConfigurationController::class, 'afterExtensionConfigurationWrite', \TYPO3\CMS\FluidStyledContent\Hooks\TcaCacheClearing::class, 'clearTcaCache'
+                );
+            }, $_EXTKEY
     );
 }
 
